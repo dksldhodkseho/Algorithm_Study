9774e3dec6c8e3fa201ea103ba90d63b6fedb1fe
dksldhodkseho <156566854+dksldhodkseho@users.noreply.github.com>
2025-05-27 20:07:05 +0900

    feat: Update 프로그래머스_같은 숫자는 싫어

--- Stack_Queue_Deque/프로그래머스_같은 숫자는 싫어.py
+++ Stack_Queue_Deque/프로그래머스_같은 숫자는 싫어.py
@@ -2,7 +2,7 @@ def solution(arr):
     answer = []
     arr.reverse() #배열 뒤집기
     for i in range(len(arr)):
-        num = arr.pop() #원소를 꺼내어 저장
+        num = arr.pop() #마지막 원소를 꺼내어 저장
         if  len(answer) == 0 or num != answer[-1]:
             answer.append(num) # answer배열에 추가
     return answer
