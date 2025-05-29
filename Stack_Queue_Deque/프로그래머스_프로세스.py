92c7d5916e43991f082c92921079ba68ddf67609
dksldhodkseho <156566854+dksldhodkseho@users.noreply.github.com>
2025-05-29 23:45:25 +0900

    feat: 프로그래머스_프로세스

---
+++ Stack_Queue_Deque/프로그래머스_프로세스.py
@@ -0,0 +1,25 @@
+from collections import deque
+def solution(priorities, location):
+    answer = 1
+    temp = []
+    dq = deque(enumerate(priorities)) #enumerate()를 통해 튜플 형태로 저장
+    while dq:
+        pro = dq.popleft() #앞에서부터 차례대로 원소 꺼내기
+        cnt = 0
+        for i in dq:
+            if pro[1] < i[1]: #우선순위가 더 높은게 있다면 다시 deque에 저장
+                dq.append(pro)
+                break
+            else:
+                cnt += 1
+        if cnt == len(dq): #우선순위가 더 높은게 없다면 temp에 저장
+            temp.append(pro)
+        
+    for i in temp: #temp배열에서 i[0] == location인 값 찾기
+        if i[0] == location:
+            break
+        answer += 1
+    
+    return answer
+profile
+G the 
